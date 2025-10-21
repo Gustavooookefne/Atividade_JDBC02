80c7d2cd72ca29495b3caf9271fb8fdfb04b9604
Gustavooookefne <gustavooookefne14@gmail.com>
2025-10-21 14:59:43 -0300

    feat: Criando o MateriaÃl

--- Atividade_JDBC02/src/main/java/org/example/Model/Material.java
+++ Atividade_JDBC02/src/main/java/org/example/Model/Material.java
@@ -1,4 +1,42 @@
 package org.example.Model;
 
 public class Material {
+    private int id;
+    private String nome;
+    private String cnpj;
+
+    public Material(int id ,String nome ,String cnpj){
+        this.id = id;
+        this.nome = nome;
+        this.cnpj = cnpj;
+    }
+
+    public Material(String nome ,String cnpj){
+        this.nome = nome;
+        this.cnpj = cnpj;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
 }
