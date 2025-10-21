0b052e52a3b7218b7c75214dc013708f584a2527
Gustavooookefne <gustavooookefne14@gmail.com>
2025-10-21 15:16:01 -0300

    feat: Finalizado o Conexao

--- Atividade_JDBC02/src/main/java/org/example/Dao/MaterialDao.java
+++ Atividade_JDBC02/src/main/java/org/example/Dao/MaterialDao.java
@@ -1,3 +1,18 @@
 package org.example.Dao;
 
+import org.example.Model.Material;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class MaterialDao {
+    public static void inserirMaterial(Material material)throws SQLException{
+        String query = """
+                INSERT INTO material
+                (nome , cnpj) VALUES (?,?)
+                """;
+
+        try(Connection conn = Conexao)
+    }
+
 }
--- Atividade_JDBC02/src/main/java/org/example/Infra/Conexao.java
+++ Atividade_JDBC02/src/main/java/org/example/Infra/Conexao.java
@@ -1,4 +1,15 @@
 package org.example.Infra;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Conexao {
+        private static final String URL = "jdbc:mysql://localhost:3306/ManutencaoIndustrial?useSSL=false&serverTimezone=UTC";
+        private static final String USER = "root";
+        private static final String PASSWORD = "mysqlPW";
+
+        public static Connection getConnection() throws SQLException{
+            return DriverManager.getConnection(URL ,USER ,PASSWORD);
+        }
 }
