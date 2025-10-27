bc8519fd02873b63e64b06457490953f766172ce
Gustavooookefne <gustavooookefne14@gmail.com>
2025-10-27 20:30:00 -0300

    feat: Comeeçando o NotaEntradaDao

--- src/main/java/org/example/Dao/NotaEntradaDao.java
+++ src/main/java/org/example/Dao/NotaEntradaDao.java
@@ -2,9 +2,12 @@ package org.example.Dao;
 
 import org.example.Model.NotaEntrada;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class NotaEntradaDao{
-
+    public void MostrarNotasEntradas(NotaEntrada notaEntrada)throws SQLException{
+        String query
+    }
 }
